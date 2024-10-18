0c03a6c2091f99e8497b3071a30b415c86372f86
NTZ9008 <131684214+NTZ9008@users.noreply.github.com>
2024-10-18 19:23:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# YOUR CODE HERE
+def summation():
+    updated_list = []
+    for i in range(n):
+        updated_list.append(list1[i]+list2[i])
+    return updated_list
+def find_min_max():
+    x = min(summation())
+    y = max(summation())
+    return (x,y)
+list1 = []
+list2 = []
+n = int(input())
+for i in range(n):
+    x = int(input())
+    list1.append(x)
+for i in range(n):
+    x = int(input())
+    list2.append(x)
+print(summation())
+print(find_min_max())
